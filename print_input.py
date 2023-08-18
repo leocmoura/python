bc5c83a4502ceff49f9cf0f0410561ec1ef89d7f
Leonardo <leocordova96@gmail.com>
2023-08-18 11:54:40 -0300

    print_input

---
+++ print_input.py
@@ -0,0 +1,7 @@
+nome = input('informe seu nome: ')
+idade = input('informe sua idade: ')
+print(nome, idade)
+print(nome, idade, end="...\n")
+print('teste')
+print(nome, idade, sep="\n") #sep=" "
+
