b336821004139a0bc74d7be77c0415b2bc86ed72
Leonardo <leocordova96@gmail.com>
2023-08-18 10:51:43 -0300

    data types, interactive mode, dir()/help()

---
+++ data_types.py
@@ -0,0 +1,17 @@
+print(1+10) # int
+print(1.5) # float
+print(1.5 + 1 + 0.5) # float
+print(True) # bool
+print(False) # bool
+print("Python") # str 
+
+int()
+float()
+str()
+bool()
+
+
+# modo interativo no terminal $ python
+dir() # ver lista metodos que podemos executar dos objs
+
+help() # sistema de ajuda integrado
