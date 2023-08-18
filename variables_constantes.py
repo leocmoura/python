89c9ad39e06f8c4346d612228e438865a91b57e1
Leonardo <leocordova96@gmail.com>
2023-08-18 11:44:17 -0300

    variables and constants

---
+++ variables_constantes.py
@@ -0,0 +1,16 @@
+nome = 'Leonardo'
+idade = 26
+
+print(nome, idade)
+
+nome, idade = 'Lia', 23
+
+print(nome, idade)
+
+#snake_case
+limite_saque_diario = 1000
+
+#constantes
+BRAZILIAN_STATES = ['SP', 'RJ', 'DF']
+
+print(BRAZILIAN_STATES)
