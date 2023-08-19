77e77f6c295e900c33565015ba3cb83bc3120936
Leonardo <leocordova96@gmail.com>
2023-08-19 10:22:32 -0300

    association operators

---
+++ association_operators.py
@@ -0,0 +1,10 @@
+frutas = ['limao', 'uva', 'Laranja']
+
+print('laranja' in frutas) # maiusculas fazem diferença na operação
+print('laranja' not in frutas)
+print('limao' in frutas)
+
+curso = 'Curso de Python'
+
+print('Python' in curso)
+print('C' not in curso)
