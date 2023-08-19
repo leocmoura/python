560d39b4733b73de0663971f1e2c68abf905d32f
Leonardo <leocordova96@gmail.com>
2023-08-19 09:46:33 -0300

    comparison oparators

---
+++ comparison_operators.py
@@ -0,0 +1,15 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque) # igual
+#True
+print(saldo != saque) # diferente
+#False
+print(saldo > saque) # maior
+#False
+print(saldo >= saque) # maior ou igual
+#True
+print(saldo < saque) # menor
+#False
+print(saldo <= saque) # menor ou igual
+#True
