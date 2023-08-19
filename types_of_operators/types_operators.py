0759e2a361dd7eb803f656821f81913bacbcb44e
Leonardo <leocordova96@gmail.com>
2023-08-19 09:42:34 -0300

    types operators

---
+++ types_of_operators/types_operators.py
@@ -0,0 +1,19 @@
+# operators arithmetic
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2) # descarta os decimais
+print(produto_1 * produto_2)
+print(produto_1 % produto_2) # mostra o resto/sobra
+print(produto_1 ** produto_2) # exponenciação
+
+x = 10 + 5 * 4
+print(x)
+x = (10 + 5) * 4
+print(x)
+y = 10 / 2 + 25 * 2 - 2 ** 2 # precedencia maior
+y = (10 / 2) + (25 * 2) - (2 ** 2) # precedencia maior
+print(y)
