a18a2aad91e7f1d0d2b7f7caacdf41ed2068241d
Leonardo <leocordova96@gmail.com>
2023-08-22 09:30:01 -0300

    tuple

---
+++ data_structures/tuples.py
@@ -0,0 +1,22 @@
+# Estrutura imutável
+
+frutas = ('laranja', 'pera', 'uva')
+frutas[0]
+frutas[2]
+frutas[-1]
+
+# Tuplas aninhadas
+
+matriz = (
+    (1, 'a', 2),
+    ('b', 3, 4),
+    (5, 6, 'c'),
+)
+
+matriz[0]
+matriz[1][0]
+
+# Fatiamento igual listas
+# .count
+# .index
+# .len
